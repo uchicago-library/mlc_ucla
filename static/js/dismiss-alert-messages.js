7632eb1b9c9b9e960c00d561d35cbdc92723bcd1
Vitor Gonçalves <vitorg@uchicago.edu>
2024-01-31 23:39:35 -0600

    add dismissible message about access to panopto time

--- static/js/dismiss-alert-messages.js
+++ static/js/dismiss-alert-messages.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var user_closed_c = parseInt(localStorage.getItem('timestamp_closed_campus_message'));
 	var user_closed_r = parseInt(localStorage.getItem('timestamp_closed_restricted_message'));
 	var current = new Date().getTime();
-	var enough_time = 6000;
+	var enough_time = 7889400000;
 	var enough_c_time = user_closed_c && current>(user_closed_c+enough_time);
 	var enough_r_time = user_closed_r && current>(user_closed_r+enough_time);
 	
