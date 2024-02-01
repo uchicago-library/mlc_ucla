01074af3cc03e43becaeb37c51ade1471a9005c9
Vitor Gonçalves <vitorg@uchicago.edu>
2024-02-01 13:16:15 -0600

    add a help button to surface the messages

--- static/js/dismiss-alert-messages.js
+++ static/js/dismiss-alert-messages.js
@@ -1,31 +1,47 @@
 $(document).ready(function(){
-	var user_closed_c = parseInt(localStorage.getItem('timestamp_closed_campus_message'));
-	var user_closed_r = parseInt(localStorage.getItem('timestamp_closed_restricted_message'));
-	var current = new Date().getTime();
+	var pdata = $('#panopto-data').data();
 	var enough_time = 7889400000;
-	var enough_c_time = user_closed_c && current>(user_closed_c+enough_time);
-	var enough_r_time = user_closed_r && current>(user_closed_r+enough_time);
-	
-	if( user_closed_c && !enough_c_time){
-		$('#alert-campus').alert('close');
-	}else if( (user_closed_c && enough_c_time) || !user_closed_c ){
-		$('#alert-campus').on('closed.bs.alert', function () {
-			localStorage.setItem('timestamp_closed_campus_message', new Date().getTime())
-		})
-		if ( enough_c_time ){
-			localStorage.removeItem('timestamp_closed_campus_message')
-		}
-	}
+	var current = new Date().getTime();
+	if(pdata.identifier){
 
-	if( user_closed_r && !enough_r_time){
-		$('#alert-restricted').alert('close');
-	}else if( (user_closed_r && enough_c_time) || !user_closed_r ){
-		$('#alert-restricted').on('closed.bs.alert', function () {
-			localStorage.setItem('timestamp_closed_restricted_message', new Date().getTime())
-		})
-		if ( enough_r_time ){
-			localStorage.removeItem('timestamp_closed_restricted_message')
-		}
-	}
+		if(pdata.rights == 'campus'){
+			var user_closed_c = parseInt(localStorage.getItem('timestamp_closed_campus_message'));
+			var enough_c_time = user_closed_c && current>(user_closed_c+enough_time);
 
+			if( user_closed_c && !enough_c_time){
+				$('#panopto-help').removeClass('hidden');
+			}
+			if( (user_closed_c && enough_c_time) || !user_closed_c ){
+				$('#alert-campus').removeClass('hidden')
+				.on('closed.bs.alert', function () {
+					localStorage.setItem('timestamp_closed_campus_message', new Date().getTime());
+				});
+				localStorage.removeItem('timestamp_closed_campus_message');
+			}
+
+		}else if(pdata.rights == 'restricted'){
+			var user_closed_r = parseInt(localStorage.getItem('timestamp_closed_campus_message'));
+			var enough_r_time = user_closed_r && current>(user_closed_r+enough_time);
+
+			if( user_closed_r && !enough_r_time){
+				$('#panopto-help').removeClass('hidden');
+			}
+			if( (user_closed_r && enough_r_time) || !user_closed_r ){
+				$('#alert-restricted').removeClass('hidden')
+				.on('closed.bs.alert', function () {
+					localStorage.setItem('timestamp_closed_restricted_message', new Date().getTime());
+				});
+				localStorage.removeItem('timestamp_closed_restricted_message');
+			}
+
+		}
+		$('#panopto-help').on('click', function (){
+			console.log('click help');
+			localStorage.removeItem('timestamp_closed_campus_message');
+			localStorage.removeItem('timestamp_closed_restricted_message');
+			$('#alert-campus').removeClass('hidden');
+			$('#alert-restricted').removeClass('hidden');
+			$('#panopto-help').addClass('hidden');
+		});
+	}
 });
